5598500bd12604a867f9dc99c6716987013f6436
Jason Birch <jbirch@atlassian.com>
2023-05-02 21:06:48 +1000

    👁️👄👁️ 👁️👄👁️ 👁️👄👁️ 👁️👄👁️ 👁️👄👁️ 👁️👄👁️

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,5 @@ RUN cargo build --release
 FROM gcr.io/distroless/cc as runtime
 
 COPY --from=builder /app/target/release/rusty-trumpet /
+EXPOSE 8080
 ENTRYPOINT ["/rusty-trumpet"]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, 🎺!");
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+
+async fn hello_trumpet(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+    println!("🎺 🎺 🎺 🎺 🎺");
+    Ok(Response::new(Body::from("Hello, 🎺!")))
 }
+
+#[tokio::main]
+async fn main() {
+    println!("commencing doot");
+
+    let svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(hello_trumpet))
+    });
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    let server = Server::bind(&addr).serve(svc);
+
+    if let Err(e) = server.await {
+        eprintln!("doot doot error: {}", e);
+    }
+
+}
+
